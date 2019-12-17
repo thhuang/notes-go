94035874ef0b2558639020c915216da73a8270b0
TH Huang <tzhsn.huang@gmail.com>
2019-12-17 20:56:58 +0800

    update

--- Tutorials/39_Exercise_Slices/exercise-slices.go
+++ Tutorials/39_Exercise_Slices/exercise-slices.go
@@ -3,13 +3,13 @@ package main
 import "golang.org/x/tour/pic"
 
 func Pic(dx, dy int) [][]uint8 {
-	var img [][]uint8
-	for i := 0; i < dx; i++ {
-		var row []uint8
-		for j := 0; j < dy; j++ {
-			row = append(row, uint8(i^j))
+	img := make([][]uint8, dx)
+	for i := range img {
+		row := make([]uint8, dy)
+		for j := range row {
+			row[j] = uint8(i^j)
 		}
-		img = append(img, row)
+		img[i] = row
 	}
 	return img
 }
